ab748c9e0c1024f80c751dbab3888b068b5118e9
pian17-ai <alviancahyopambudi@gmail.com>
2026-01-13 22:49:30 +0700

    add learning

---
+++ Learning/PZN1/src/HelloWorld/HelloWorld.jsx
@@ -0,0 +1,26 @@
+export default function HelloWorld() {
+  return (
+    <div>
+      <HeaderHelloWorld />
+      <Kubo />
+    </div>
+  );
+}
+
+function HeaderHelloWorld() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Woi Selamat Belajar React</p>
+    </div>
+  );
+}
+
+function Kubo() {
+  return (
+    <div>
+      <h1>Nagisa Kubo</h1>
+      <p>Nagisa Kubo is very very beautiful and cute girl</p>
+    </div>
+  );
+}
