f18e0c3f8fe18ee200141f3379749ce8693a72d7
pian17-ai <alviancahyopambudi@gmail.com>
2026-07-14 23:48:46 +0700

    update footer

--- Try/router-layout/src/components/Footer.jsx
+++ Try/router-layout/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="w-full text-center">
-            <h1 className="text-gray-400">Pian @2025</h1>
+            <h1 className="text-gray-400">Pian @2025 1.0.0</h1>
         </footer>
     )
 }
